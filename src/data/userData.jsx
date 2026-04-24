d0491972563d9bea3b406c1f78c3a9a3bdd88848
Sanjeev Midda <s.midda1@gmail.com>
2026-04-24 22:24:27 +0100

    feat: add seed data for cm cards

---
+++ src/data/userData.jsx
@@ -0,0 +1,47 @@
+export const userData = [
+  {
+    name: "민준",
+    number: 1,
+    color: "grey",
+  },
+  {
+    name: "서준",
+    number: 2,
+    color: "grey",
+  },
+  {
+    name: "서연",
+    number: 3,
+    color: "grey",
+  },
+  {
+    name: "지우",
+    number: 4,
+    color: "grey",
+  },
+  {
+    name: "지훈",
+    number: 5,
+    color: "grey",
+  },
+  {
+    name: "현우",
+    number: 6,
+    color: "grey",
+  },
+  {
+    name: "하은",
+    number: 7,
+    color: "grey",
+  },
+  {
+    name: "민서",
+    number: 8,
+    color: "grey",
+  },
+  {
+    name: "준호",
+    number: 9,
+    color: "grey",
+  },
+];
